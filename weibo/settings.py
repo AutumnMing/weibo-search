ba0f2241e4306b4de7e0169a2e53a825e4d07968
Autumn ming <792247548@qq.com>
2024-08-14 21:31:06 +0800

    Update settings.py

--- weibo/settings.py
+++ weibo/settings.py
@@ -3,8 +3,8 @@
 BOT_NAME = 'weibo'
 SPIDER_MODULES = ['weibo.spiders']
 NEWSPIDER_MODULE = 'weibo.spiders'
-COOKIES_ENABLED = False
-TELNETCONSOLE_ENABLED = False
+COOKIES_ENABLED = True
+TELNETCONSOLE_ENABLED = True
 LOG_LEVEL = 'ERROR'
 # 访问完一个页面再访问下一个时需要等待的时间，默认为10秒
 DOWNLOAD_DELAY = 10
@@ -12,7 +12,7 @@ DEFAULT_REQUEST_HEADERS = {
     'Accept':
     'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7',
-    'cookie': 'your cookie'
+    'cookie': '''SCF=Ar-YqMUCVVaNzFS67w3HL4kPXroM9cO4Lq6QXlM7QdFS2CTpsf7ZKzeWG-hRWOEPWlGI0NsLlHPloMOjMkivy1Q.; SUB=_2A25LjIOQDeRhGeVH7lcR8ibEzzmIHXVo45lYrDV6PUJbktAbLUunkW1NT1yJYjzdIf34ewf6X9rv91DrFW2rAmtI; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WhH67O9cwlEhR8A-GTfeCVy5NHD95Q01K-fehzR1hBfWs4DqcjTKgHyUsL_wg8Xqg7t; SSOLoginState=1720251328; ALF=1722843328; _T_WM=b5d5fb83f78cd0cd933fca66863e4fd7; MLOGIN=1; WEIBOCN_FROM=1110006030'''
 }
 ITEM_PIPELINES = {
     'weibo.pipelines.DuplicatesPipeline': 300,
@@ -24,7 +24,7 @@ ITEM_PIPELINES = {
 }
 # 要搜索的关键词列表，可写多个, 值可以是由关键词或话题组成的列表，也可以是包含关键词的txt文件路径，
 # 如'keyword_list.txt'，txt文件中每个关键词占一行
-KEYWORD_LIST = ['迪丽热巴']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
+KEYWORD_LIST = ['姜萍']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
 # 要搜索的微博类型，0代表搜索全部微博，1代表搜索全部原创微博，2代表热门微博，3代表关注人微博，4代表认证用户微博，5代表媒体微博，6代表观点微博
 WEIBO_TYPE = 1
 # 筛选结果微博中必需包含的内容，0代表不筛选，获取全部微博，1代表搜索包含图片的微博，2代表包含视频的微博，3代表包含音乐的微博，4代表包含短链接的微博
@@ -33,12 +33,12 @@ CONTAIN_TYPE = 0
 # 具体支持的地名见region.py文件，注意只支持省或直辖市的名字，省下面的市名及直辖市下面的区县名不支持，不筛选请用“全部”
 REGION = ['全部']
 # 搜索的起始日期，为yyyy-mm-dd形式，搜索结果包含该日期
-START_DATE = '2020-03-01'
+START_DATE = '2024-06-01'
 # 搜索的终止日期，为yyyy-mm-dd形式，搜索结果包含该日期
-END_DATE = '2020-03-01'
+END_DATE = '2024-07-06'
 # 进一步细分搜索的阈值，若结果页数大于等于该值，则认为结果没有完全展示，细分搜索条件重新搜索以获取更多微博。数值越大速度越快，也越有可能漏掉微博；数值越小速度越慢，获取的微博就越多。
 # 建议数值大小设置在40到50之间。
-FURTHER_THRESHOLD = 46
+FURTHER_THRESHOLD = 40
 # 图片文件存储路径
 IMAGES_STORE = './'
 # 视频文件存储路径
